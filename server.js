97dcb85da07ad5df0764dd2061c1fa2ff8ef4b50
Pablo Silva <p5f8@hotmail.com>
2018-08-13 23:53:48 -0300

    Correction database name

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var express = require("express"),
 
 // mongoose instance connection url connection
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/dbdoido');
+mongoose.connect('mongodb://localhost/TodoListdb');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
